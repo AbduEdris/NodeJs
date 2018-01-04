fe3c5c4ed4cab4c3cd97c9bb6a28fd59750b0605
Abdu Endris <AbduEdris@users.noreply.github.com>
2018-01-04 13:56:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
-
-var http = require('http');
-var fs = require('fs');
-var server = http.createServer(function (req, res) {
-    
-    fs.readFile('index.html', function (err, data) {
-        res.write(data);
-        res.write("Abdu Edris  ");
-        res.end();
-
-    });
-});
-
-server.listen(8000);
+console.log("welcome to my NodeJS practical class");
