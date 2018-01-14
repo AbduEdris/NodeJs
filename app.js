b4e276fcb63a810c882b0bd3ce39c0f000234ba0
Abdu Endris <AbduEdris@users.noreply.github.com>
2018-01-14 22:59:16 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
-console.log("welcome to my NodeJS practical class");
-console.log("Please look at my NodeJS branchs");
-console.log("Your support is warmly welcome");
+
+var http = require('http');
+var fs = require('fs');
+var abdi = new Object();
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get('/',function(req,res){
+    fs.readFile('index.html', function (err, data) {
+        res.write(data);
+        res.end();
+});
+});
+
+app.get('/login',function(req,res){
+    fs.readFile('login.html', function (err, data) {
+        res.write(data);
+        res.end();
+});
+});
+
+app.get('/signup',function(req,res){
+    fs.readFile('signup.html', function (err, data) {
+        res.write(data);
+        res.end();
+});
+});
+
+app.listen(8000);
